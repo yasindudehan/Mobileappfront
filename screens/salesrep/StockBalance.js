b68fc752a99cc5048eb0bd8c8a72bfbb2e836c2d
yasindudehan <43140925+yasindudehan@users.noreply.github.com>
2020-02-16 23:36:26 +0530

    change file

--- screens/salesrep/StockBalance.js
+++ screens/salesrep/StockBalance.js
@@ -9,6 +9,10 @@ import {
   FlatList,
   PermissionsAndroid,
   Platform,
+  Dimensions,
+  ScrollView,
+  SafeAreaView,
+  AsyncStorage,
 } from 'react-native';
 import ProgressCircle from 'react-native-progress-circle';
 import Axios from 'axios';
@@ -38,19 +42,88 @@ import {
   Cell,
 } from 'react-native-table-component';
 import {TextInput} from 'react-native-gesture-handler';
+import Date from './Date';
+import {TabNavigator} from 'react-navigation';
+import Geolocation from '@react-native-community/geolocation';
+
+var array1 = [];
+var array2 = [];
+var presntage_arr = [];
 export default class StockBalanceScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: {},
+      stockBalance: [],
       isLoaded: true,
-      currentLogitude: 'unknown',
-      currentLatitude: 'unknown',
+      qut: 0,
+      quts: [],
+      price: 0,
+      prices: [],
+      totalValue: 0,
+      repName: '',
+      repInfo: [],
+      distName: '',
+      data: [],
+      stock: [],
+      isLoading: false,
     };
   }
-  async componentDidMount() {
-    await Axios.get('http://192.168.1.102:4000/product')
 
+  componentDidMount = async () => {
+    const repName = await AsyncStorage.getItem('username');
+
+    this.setState({repName: repName});
+    await Axios.post('http://192.168.1.103:4000/login/salesrep', {
+      userName: repName,
+    }).then(json => {
+      this.setState({data: json.data[0]});
+      const datas = this.state.data;
+
+      this.setState({repInfo: datas});
+      //  console.warn(this.state.repInfo.distributor);
+    });
+    const distname = this.state.repInfo.distributor;
+    AsyncStorage.setItem('distName', this.state.repInfo.distributor);
+    this.setState({distName: distname});
+    await Axios.post('http://192.168.1.103:4000/stock/getstock', {
+      repname: repName,
+      distname: distname,
+    }).then(json => {
+      const stockB = json.data[0];
+      Object.keys(stockB).map((item, i) => {
+        if (stockB[item].qut >= 0) {
+          array1[item] = stockB[item];
+        }
+      });
+      this.setState({stockBalance: array1});
+      //  console.warn(this.state.stockBalance);
+      if (this.state.stockBalance === undefined) {
+        this.setState({isLoading: false});
+      } else {
+        this.setState({isLoading: true});
+      }
+    });
+    await Axios.post('http://192.168.1.103:4000/stock/stock', {
+      repname: repName,
+      distname: distname,
+    }).then(json => {
+      const stoc = json.data[0];
+      Object.keys(stoc).map((item, i) => {
+        if (0 <= stoc[item].qut) {
+          array2[item] = stoc[item];
+        }
+      });
+
+      this.setState({stock: array2});
+
+      //console.warn(this.state.stockBalance['teapouch20']);
+    });
+    Object.keys(this.state.stock).map((item, i) => {
+      presntage_arr[item] =
+        (this.state.stockBalance[item].qut / this.state.stock[item].qut) * 100;
+    });
+
+    /*Axios.get('http://192.168.1.104:4000/product')
       .then(json => {
         this.setState({
           isLoaded: true,
@@ -59,126 +132,90 @@ export default class StockBalanceScreen extends Component {
       })
       .catch(error => {
         console.error(error);
-      });
-  }
-  componentWillUnmount = () => {
-    navigator.geolocation.clearWatch(this.watchID);
+      });*/
   };
   render() {
-    const {products} = this.state;
-    const a = 100;
-    const b = 1000;
-    if (this.state.isLoaded === false) {
-      return <ActivityIndicator />;
-    } else {
+    const {
+      products,
+      qut,
+      price,
+      quts,
+      prices,
+      totalValue,
+      repName,
+      distName,
+      repInfo,
+      stockBalance,
+      stock,
+    } = this.state;
+
+    if (this.state.isLoading === false) {
       return (
         <View>
-          <Table>
-            <View
-              style={{
-                flex: 3,
-                alignSelf: 'stretch',
-                borderWidth: 1,
-                borderColor: 'green',
-                borderBottomWidth: 0,
-                marginBottom: 50,
-                backgroundColor: 'green',
-              }}>
-              <Text>Product Name</Text>
-            </View>
-            <View
-              style={{
-                flex: 3,
-                flexDirection: 'row',
-                alignSelf: 'stretch',
-                borderWidth: 1,
-                borderColor: 'green',
-                borderBottomWidth: 0,
-                marginBottom: 50,
-              }}>
-              <View
-                style={{
-                  flex: 2,
-                  alignSelf: 'stretch',
-                  borderWidth: 1,
-                  borderColor: 'green',
-                  borderBottomWidth: 0,
-                }}>
-                <Text>Weight</Text>
-              </View>
-              <View
-                style={{
-                  flex: 1,
-                  alignSelf: 'stretch',
-                  borderWidth: 1,
-                  borderColor: 'green',
-                  borderBottomWidth: 0,
-                }}>
-                <Text>Qut</Text>
-              </View>
-              <View
-                style={{
-                  flex: 1,
-                  alignSelf: 'stretch',
-                  borderWidth: 1,
-                  borderColor: 'green',
-                  borderBottomWidth: 0,
-                }}>
-                <Text>Rate</Text>
-              </View>
-              <View
-                style={{
-                  flex: 1,
-                  alignSelf: 'stretch',
-                  borderWidth: 1,
-                  borderColor: 'green',
-                  borderBottomWidth: 0,
-                }}>
-                <Text>Value</Text>
-              </View>
-            </View>
+          <ActivityIndicator />
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <Container style={styles.container}>
+            <Content padder>
+              {Object.keys(stock).map((item, i) => {
+                return (
+                  <Card>
+                    <Body>
+                      <Text style={styles.TextStyle1} key={i}>
+                        {stockBalance[item].name}
+                      </Text>
+                    </Body>
+                    <Body>
+                      <Text key={i} style={styles.TextStyle1}>
+                        {stockBalance[item].weight}
+                      </Text>
+                    </Body>
 
-            {Object.keys(products).map((p, i) => {
-              return (
-                <View>
-                  <View style={{backgroundColor: 'green'}}>
-                    <Text key={{i}}>{products[p].name}</Text>
-                  </View>
-                  <View
-                    style={{
-                      flexDirection: 'row',
-                      alignSelf: 'stretch',
-                      borderWidth: 1,
-                      borderColor: 'green',
-                      borderBottomWidth: 0,
-                    }}>
-                    <View style={{flex: 2}}>
-                      <Text key={i}>{products[p].weight}</Text>
-                    </View>
-                    <View style={{flex: 1}}>
-                      <TextInput placeholder="____" key={i} />
-                    </View>
-                    <View style={{flex: 1}}>
-                      <Text key={i}>{products[p].rate}</Text>
-                    </View>
-                    <View style={{flex: 1}}>
-                      <Text key={i}>-------</Text>
-                    </View>
-                  </View>
-                </View>
-              );
-            })}
-            <Text>{this.state.initialPosition}</Text>
-            <Text></Text>
-          </Table>
+                    <CardItem style={{backgroundColor: '#00363a', height: 250}}>
+                      <ProgressCircle
+                        key={i}
+                        style={{flex: 1}}
+                        percent={
+                          stock[item].qut !== 0
+                            ? (stockBalance[item].qut / stock[item].qut) * 100
+                            : 0
+                        }
+                        radius={100}
+                        borderWidth={5}
+                        color="#a19b03"
+                        shadowColor="#131414"
+                        bgColor="#00363a">
+                        <Text style={styles.TextStyle} key={i}>
+                          {stockBalance[item].qut}
+                        </Text>
+                      </ProgressCircle>
+                    </CardItem>
+                  </Card>
+                );
+              })}
+            </Content>
+          </Container>
         </View>
       );
     }
   }
 }
+
 const styles = (StyleSheet.stockBalance = {
   container: {
     flex: 1,
-    backgroundColor: '#00363a',
+  },
+  TextStyle: {
+    fontSize: 25,
+    fontFamily: 'roboto',
+    color: 'white',
+  },
+  TextStyle1: {
+    fontSize: 25,
+    fontFamily: 'roboto',
+    color: '#00363a',
   },
 });
