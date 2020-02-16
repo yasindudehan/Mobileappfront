a830071b7c988779cbc4314696e2391fc16b43bb
yasindudehan <43140925+yasindudehan@users.noreply.github.com>
2020-02-16 23:37:35 +0530

    change file

--- screens/salesrep/StockBalance.js
+++ screens/salesrep/StockBalance.js
@@ -46,10 +46,9 @@ import Date from './Date';
 import {TabNavigator} from 'react-navigation';
 import Geolocation from '@react-native-community/geolocation';
 
-var quts_arr = [];
-var rate_arr = [];
-var price_arr = [];
-var total = 0;
+var array1 = [];
+var array2 = [];
+var presntage_arr = [];
 export default class StockBalanceScreen extends Component {
   constructor(props) {
     super(props);
@@ -64,28 +63,66 @@ export default class StockBalanceScreen extends Component {
       repName: '',
       repInfo: [],
       distName: '',
+      data: [],
+      stock: [],
+      isLoading: false,
     };
   }
 
   componentDidMount = async () => {
     const repName = await AsyncStorage.getItem('username');
+
     this.setState({repName: repName});
-    await Axios.post('http://192.168.1.104:4000/login/salesrep', {
+    await Axios.post('http://192.168.1.103:4000/login/salesrep', {
       userName: repName,
     }).then(json => {
-      this.setState({repInfo: json.data[0]});
+      this.setState({data: json.data[0]});
+      const datas = this.state.data;
 
+      this.setState({repInfo: datas});
       //  console.warn(this.state.repInfo.distributor);
     });
     const distname = this.state.repInfo.distributor;
+    AsyncStorage.setItem('distName', this.state.repInfo.distributor);
     this.setState({distName: distname});
-    await Axios.post('http://192.168.1.104:4000/stock/getstock', {
+    await Axios.post('http://192.168.1.103:4000/stock/getstock', {
       repname: repName,
       distname: distname,
     }).then(json => {
-      this.setState({stockBalance: json.data[0]});
+      const stockB = json.data[0];
+      Object.keys(stockB).map((item, i) => {
+        if (stockB[item].qut >= 0) {
+          array1[item] = stockB[item];
+        }
+      });
+      this.setState({stockBalance: array1});
+      //  console.warn(this.state.stockBalance);
+      if (this.state.stockBalance === undefined) {
+        this.setState({isLoading: false});
+      } else {
+        this.setState({isLoading: true});
+      }
+    });
+    await Axios.post('http://192.168.1.103:4000/stock/stock', {
+      repname: repName,
+      distname: distname,
+    }).then(json => {
+      const stoc = json.data[0];
+      Object.keys(stoc).map((item, i) => {
+        if (0 <= stoc[item].qut) {
+          array2[item] = stoc[item];
+        }
+      });
+
+      this.setState({stock: array2});
+
       //console.warn(this.state.stockBalance['teapouch20']);
     });
+    Object.keys(this.state.stock).map((item, i) => {
+      presntage_arr[item] =
+        (this.state.stockBalance[item].qut / this.state.stock[item].qut) * 100;
+    });
+
     /*Axios.get('http://192.168.1.104:4000/product')
       .then(json => {
         this.setState({
@@ -109,11 +146,10 @@ export default class StockBalanceScreen extends Component {
       distName,
       repInfo,
       stockBalance,
+      stock,
     } = this.state;
-    const a = 100;
-    const b = 1000;
 
-    if (this.state.isLoaded === false) {
+    if (this.state.isLoading === false) {
       return (
         <View>
           <ActivityIndicator />
@@ -124,13 +160,11 @@ export default class StockBalanceScreen extends Component {
         <View style={styles.container}>
           <Container style={styles.container}>
             <Content padder>
-              {Object.keys(stockBalance).map((item, i) => {
-                quts_arr[item] = parseInt(stockBalance[item].qut);
-
+              {Object.keys(stock).map((item, i) => {
                 return (
                   <Card>
                     <Body>
-                      <Text style={styles.TextStyle1}>
+                      <Text style={styles.TextStyle1} key={i}>
                         {stockBalance[item].name}
                       </Text>
                     </Body>
@@ -142,15 +176,20 @@ export default class StockBalanceScreen extends Component {
 
                     <CardItem style={{backgroundColor: '#00363a', height: 250}}>
                       <ProgressCircle
+                        key={i}
                         style={{flex: 1}}
-                        percent={((b - a) / b) * 100}
+                        percent={
+                          stock[item].qut !== 0
+                            ? (stockBalance[item].qut / stock[item].qut) * 100
+                            : 0
+                        }
                         radius={100}
                         borderWidth={5}
                         color="#a19b03"
                         shadowColor="#131414"
                         bgColor="#00363a">
-                        <Text style={styles.TextStyle}>
-                          {((b - a) / b) * 100}%
+                        <Text style={styles.TextStyle} key={i}>
+                          {stockBalance[item].qut}
                         </Text>
                       </ProgressCircle>
                     </CardItem>
